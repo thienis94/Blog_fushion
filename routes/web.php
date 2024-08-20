a613b3a5736e7b87a7ac9821424b4f095ba49f3c
thienis94 <thierrynisengwe3@gmail.com>
2024-08-20 17:23:59 +0200

    good

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
-use App\Http\Controllers\BlogController;
+use App\Http\Controllers\fushionController;
 
-Route::get('/', [BlogController::class, 'home'])->name('home');
-Route::get('/about', [BlogController::class, 'aboutUs'])->name('about');
-Route::get('/news', [BlogController::class, 'news'])->name('news');
-Route::get('/contact', [BlogController::class, 'contactUs'])->name('contact');
+Route::get('/', [fushionController::class, 'home'])->name('home');
+Route::get('/about', [fushionController::class, 'aboutUs'])->name('about');
+Route::get('/news', [fushionController::class, 'news'])->name('news');
+Route::get('/contact', [fushionController::class, 'contactUs'])->name('contact');
 
 // Routes pour la partie admin
-Route::get('/login', [BlogController::class, 'login'])->name('login');
-Route::get('/register', [BlogController::class, 'register'])->name('register');
+Route::get('/login', [fushionController::class, 'login'])->name('login');
+Route::get('/register', [fushionController::class, 'register'])->name('register');
